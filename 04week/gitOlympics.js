aa0be6983c97e1e87add59f56d9b7698f67db568
Alan Houston <alanhouston5000@gmail.com>
2018-10-25 18:57:53 -0500

    first commit

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const printListofOlympians = (arr) =>{
+    arr.forEach(element => {
+        console.log(element)
+    });
+}
+
+printListofOlympians('Charlie','Mac','Dennis','Dee','Frank');
