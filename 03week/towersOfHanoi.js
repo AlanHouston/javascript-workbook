1a7bedfbdc778171bddd285402db79b969fd8673
Alan Houston <alanhouston5000@gmail.com>
2018-10-26 15:56:23 -0500

    begin basic functions

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -7,11 +7,19 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+//grab one piece from the end each time - movePiece(), use pop()
+//check if move is legal, ie empty stack or last piece is bigger - isLegal()
+//if isLegal, push()
+//checkforwin() - when stacks.c = [4,3,2,1] - you win a cookie!
+//resetStack() - stacks.a = [4,3,2,1]
+
 let stacks = {
   a: [4, 3, 2, 1],
   b: [],
   c: []
 };
+
+console.log(stacks.c)
 
 function printStacks() {
   console.log("a: " + stacks.a);
@@ -19,23 +27,33 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
-  // Your code here
+const movePiece = (startStack,endStack)=> {
+  let piece = stacks.startStack.pop();
+  stacks.endStack.push(piece);
+}
+
+const isLegal = (piece)=> {
+  let lastPiece = stacks.endStack.length-1;
+  if(stacks.endStack == [] || stacks.endStack.lastPiece<piece) {
+    stacks.endStack.push(piece);
+  }
+}
+
+const checkForWin = ()=> {
+  return stacks.c = [4,3,2,1];
 
 }
 
-function isLegal() {
-  // Your code here
-
-}
-
-function checkForWin() {
-  // Your code here
-
+const resetStack = ()=> {
+  stacks = {
+    a: [4, 3, 2, 1],
+    b: [],
+    c: []
+  };
 }
 
 function towersOfHanoi(startStack, endStack) {
-  // Your code here
+  
 
 }
 
