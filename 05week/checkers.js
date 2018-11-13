610d9b6439fcc534169206a12f856a4abb4cf6f8
Alan Houston <alanhouston5000@gmail.com>
2018-11-12 20:32:00 -0600

    starting checkers on board

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -1,3 +1,14 @@
+//split input into two separate numbers, indexes
+//even rows are odd, odd rows are even
+//for loop to fill in pieces for each 
+
+//put checkers on board, 12 R, 12 W
+//make space null after piece moves
+//isMoveValid()
+//switchPlayers(
+//checkForWin()
+
+
 'use strict';
 
 const assert = require('assert');
@@ -7,15 +18,19 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+//changed this from function Checker() - was that old syntax or what??
+class Checker {
+  constructor(symbol){
+    this.symbol = symbol;
+  }
 
-function Checker() {
-  // Your code here
 }
 
 
 class Board {
   constructor() {
-    this.grid = []
+    this.grid = [];
+    this.checkers = []
   }
   // method that creates an 8x8 array, filled with null values
   createGrid() {
@@ -52,8 +67,31 @@ class Board {
     }
     console.log(string);
   }
-
-  // Your code here
+  
+  isInputValid(twoDigitInput){
+    const row = twoDigitInput[0];
+    const column = twoDigitInput[1];
+    return twoDigitInput.length == 2 && row >= 0 && row <= 7 && column >= 0 && column <=7;
+  }
+  
+  createCheckers(){
+    const xPositions = [[0, 1], [0, 3], [0, 5], [0, 7],
+    [1, 0], [1, 2], [1, 4], [1, 6],
+    [2, 1], [2, 3], [2, 5], [2, 7]];
+    const oPositions = [[5, 0], [5, 2], [5, 4], [5, 6],
+    [6, 1], [6, 3], [6, 5], [6, 7],
+    [7, 0], [7, 2], [7, 4], [7, 6]];
+    for(let x=0;x<12;x++){
+      const xChecker = new Checker('X');
+      this.checkers.push(xChecker);
+      this.grid[xPositions[x][0]][xPositions[x][1]] = this.checkers[x];
+    }
+    for(let o=0;o<12;o++){
+      const oChecker = new Checker('O');
+      this.checkers.push(oChecker);
+      this.grid[oPositions[o][0]][oPositions[o][1]] = this.checkers[o + 12];
+    }
+  }
 }
 
 class Game {
@@ -62,6 +100,7 @@ class Game {
   }
   start() {
     this.board.createGrid();
+    this.board.createCheckers();
   }
 }
 
@@ -78,7 +117,6 @@ function getPrompt() {
 const game = new Game();
 game.start();
 
-
 // Tests
 if (typeof describe === 'function') {
   describe('Game', () => {
@@ -93,7 +131,7 @@ if (typeof describe === 'function') {
   describe('Game.moveChecker()', () => {
     it('should move a checker', () => {
       assert(!game.board.grid[4][1]);
-      game.moveChecker('50', '41');
+      game.moveChecker('50', '41');//these are not numbers, but 5,0 & 4,1
       assert(game.board.grid[4][1]);
       game.moveChecker('21', '30');
       assert(game.board.grid[3][0]);
